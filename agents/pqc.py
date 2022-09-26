2f1c95bbda3618620c6a69adfd428254f9c50df2
albertomanzano <alberto.manzano.herrero@gmail.com>
2022-09-26 19:33:11 +0200

    Multiple input version working

--- agents/pqc.py
+++ agents/pqc.py
@@ -1,174 +1,200 @@
-# Update package resources to account for version changes.
-import importlib, pkg_resources
-importlib.reload(pkg_resources)
+import pennylane as qml
+import matplotlib.pyplot as plt
+from pennylane.optimize import AdamOptimizer, GradientDescentOptimizer
+from pennylane import numpy as np
 
-import tensorflow as tf
-import tensorflow_quantum as tfq
+class PQC:
 
-import cirq
-import sympy
-import numpy as np
-
-class PQC():
-    def __init__(self,input_space = 2,output_space = 1,n_layers = 2, arquitecture: str = None):
-        self.input_space = input_space
-        self.output_space = output_space
-        self.n_qubits = input_space+output_space
+    def __init__(self,n_inputs: int = 1,n_outputs: int = 1,n_layers: int = 1, simulator: str = "forest.numpy_wavefunction"):
         self.n_layers = n_layers
-
-        # Build the circuit
-        self.circuit = cirq.Circuit()
-        self.qubits = cirq.GridQubit.rect(1,self.n_qubits)
-        if (arquitecture=="init1"):
-            self.init_1()
-        elif (arquitecture=="init2"):
-            self.init_2()
-        elif (arquitecture=="init3"):
-            self.init_3()
-        elif (arquitecture=="init4"):
-            self.init_4()
-        elif (arquitecture=="init5"):
-            self.init_5()
-        else:
-            self.init_6()
-
-
-        self.readout = cirq.Z(self.qubits[-1])
+        self.n_inputs = n_inputs
+        self.n_outputs = n_outputs
+        self.weights = np.random.uniform(size = (self.n_inputs+self.n_outputs,self.n_layers),requires_grad = True)
         
-        
-        # Build the Keras model.
-        self.model = tf.keras.Sequential([
-            # The input
-            tf.keras.layers.Input(shape=(), dtype=tf.string),
-            # The PQC layer returns the expected value of the readout gate, range [-1,1].
-            tfq.layers.PQC(self.circuit, self.readout),
-        ])
+        #self.dev = qml.device("forest.qvm", device="{}q-pyqvm".format(n_inputs+n_outputs), shots = 1024)
+        self.dev = qml.device(simulator, wires=self.n_inputs+self.n_outputs)
+        self.circuit = qml.QNode(self.arquitecture,self.dev)
+
+        # Training
+        self.optimizer = None
+
+
+    def arquitecture(self,weights,x):
+        """A variational quantum circuit representing the Universal classifier.
+
+        Args:
+            params (array[float]): array of parameters
+            x (array[float]): single input vector
+
+        Returns:
+            float: fidelity between output state and input
+        """
+        # Initialization
+        for i in range(self.n_inputs+self.n_outputs):
+            qml.Hadamard(wires = i)
+
+        for l in range(self.n_layers):
+            
+            # Variational layer
+            for i in range(self.n_inputs+self.n_outputs):
+                qml.Rot(0,0,weights[i,l],wires=i)
+            
+            # Encoding layer
+            for i in range(self.n_inputs):
+                qml.Rot(x[i],x[i],x[i],wires=i)
+                
+            # Entangling layer
+            for i in range(self.n_inputs+self.n_outputs-1):
+                qml.CNOT(wires = [i,i+1])
+            qml.CNOT(wires = [self.n_inputs+self.n_outputs-1,0])
+
+        # Operator
+        #op = qml.prod(*[ qml.PauliX(wires = i) for i in range(self.n_inputs) ])
+        op = qml.PauliX(wires = self.n_inputs+self.n_outputs-1)
+        return qml.expval(op)
+
+    def call(self,weights,x):
+        ''' Processes a single input'''
+        x = np.array(x,requires_grad = True)
+        y = self.circuit(weights,x)
+        y_grad = qml.gradients.param_shift(self.circuit)(weights,x)
+        solution = np.hstack([y,y_grad[1]])
+        return solution
     
-    def atan_mapping(self,x):
-        return np.arctan(x)/np.pi*2
-
-    def angle_encoding(self,x):
-        """Encode truncated classical image into quantum datapoint."""
-        qubits = cirq.GridQubit.rect(1, self.n_qubits)
-        angle = np.arcsin(x)
-        circuit = cirq.Circuit()
-        for i in range(len(angle)):
-            circuit.append(cirq.ry(angle[i]).on(qubits[i]))
-        return circuit
-
-    def convert_to_circuit(self,x):
-        x_normalised = self.atan_mapping(x)
-        circuit = self.angle_encoding(x_normalised)
-
-        return circuit
-
-    def layer_single(self,gate,layer_number):
-        for i in range(self.input_space):
-            symbol = sympy.Symbol(str(layer_number) + '-' + str(i))
-            self.circuit.append(gate(symbol).on(self.qubits[i]))
-
-    def output_entangling(self,gate):
-        for i in range(self.input_space):
-            for j in range(self.output_space):
-                self.circuit.append(gate(self.qubits[i],self.qubits[self.input_space+j]))
-
-    def cyclic_entangling(self,gate):
-        self.circuit.append( [ gate(q0, q1) for q0, q1 in zip(self.qubits, self.qubits[1:])])
-        self.circuit.append( gate(self.qubits[-1], self.qubits[0]) )
-
-    def init_1(self):
-        for i in range(self.n_layers):
-            self.layer_single(cirq.ry,2*i)
-            self.output_entangling(cirq.CNOT)
-        self.number_parameters = self.n_layers*self.n_qubits
-    
-    def init_2(self):
-        # Output entagling and same parameter for rx,ry,rz
-        for i in range(self.n_layers):
-            self.layer_single(cirq.ry,i)
-            self.layer_single(cirq.rx,i)
-            self.layer_single(cirq.rz,i)
-            self.output_entangling(cirq.CNOT)
-        self.number_parameters = self.n_layers*self.n_qubits
-    
-    def init_3(self):
-        # Cyclic entagling and same parameter for rx,ry,rz
-        for i in range(self.n_layers):
-            self.layer_single(cirq.ry,i)
-            self.layer_single(cirq.rx,i)
-            self.layer_single(cirq.rz,i)
-            self.cyclic_entangling(cirq.CNOT)
-        self.number_parameters = self.n_layers*self.n_qubits
-    
-    def init_4(self):
-        # Cyclic entagling and same parameter for rx,ry,rz
-        for i in range(self.n_layers):
-            self.layer_single(cirq.ry,3*i)
-            self.layer_single(cirq.rx,3*i+1)
-            self.layer_single(cirq.rz,3*i+2)
-            self.cyclic_entangling(cirq.CNOT)
-        self.number_parameters = self.n_layers*self.n_qubits
-    
-    def init_5(self):
-        # Cyclic entagling and same parameter for rx,ry,rz
-        self.circuit.append(cirq.H(self.qubits[-1]))
-        for i in range(self.n_layers):
-            self.layer_single(cirq.ry,3*i)
-            self.layer_single(cirq.rx,3*i+1)
-            self.layer_single(cirq.rz,3*i+2)
-            self.cyclic_entangling(cirq.CNOT)
-        self.number_parameters = self.n_layers*self.n_qubits
-        self.circuit.append(cirq.H(self.qubits[-1]))
-    
-    def init_6(self):
-        # Cyclic entagling and same parameter for rx,ry,rz
-        for i in range(self.n_layers):
-            self.layer_single(cirq.ry,3*i)
-            self.layer_single(cirq.rx,3*i+1)
-            self.layer_single(cirq.rz,3*i+2)
-            for j in range(self.input_space):
-                self.circuit.append(cirq.H(self.qubits[j]))
-            self.cyclic_entangling(cirq.CNOT)
-        self.number_parameters = self.n_layers*self.n_qubits
-
-    def print_circuit(self):
-        print(self.circuit)
-        return None
-    
- 
-    def fit(self,x_train,y_train,x_test,y_test,epochs = 2):
-        self.model.compile(
-            loss=tf.keras.losses.MeanAbsoluteError(),
-            optimizer=tf.keras.optimizers.Adam(),
-            metrics=[tf.keras.metrics.MeanAbsoluteError()])
-
-        print(self.model.summary())
-
-        qnn_history = self.model.fit(
-            x_train, y_train,
-            batch_size=32,
-            epochs=epochs,
-            verbose=1,
-            validation_data=(x_test, y_test))
-
-        return qnn_history
-
     def predict(self,x):
-        d = len(x)
-        d_i = [ len(element) for element in x ]
-        grid = np.array(np.meshgrid(*x)).T.reshape(-1,d)
-        grid_size = len(grid)
-        circuits = [ self.convert_to_circuit(element) for element in grid ] 
-        tensor = tfq.convert_to_tensor(circuits)
-        prediction = self.model.predict(tensor)
-        prediction = prediction.reshape(d_i).T
+        ''' Processes an array of inputs'''
+        y = np.zeros((len(x),self.n_inputs+self.n_outputs),requires_grad = False)
+        for i in range(len(x)):
+            y[i] = self.call(self.weights,x[i])
+        
+        return y
 
-        return prediction
+    def compile(self,optimizer = None, loss = None, metrics = None, loss_weights = None):
+        if optimizer is None:
+            optimizer = AdamOptimizer(0.06, beta1=0.9, beta2=0.999) 
+        self.optimizer = optimizer
 
-    def __call__(self,x):
-        return self.predict(x)
-       
+    def L2(self,y1,y2):
+        return np.power(y1-y2,2.0)
+    
+    def cost(self,weights,inputs, outputs,loss_weights):
+        """Cost function to be minimized.
 
+        Args:
+            x (array[float]): array of input vectors
+            y (array[float]): array of targets
 
+        Returns:
+            float: loss value to be minimized
+        """
+        # Compute prediction for each input in data batch
+        loss = 0.0
+        for i, x in enumerate(inputs):
+            pred   = self.call(weights,x)
+            loss_i = self.L2(outputs[i],pred)
+            for j in range(self.n_inputs+1):
+                loss = loss + loss_weights[j]*loss_i[j]
+        return loss
+     
+
+    def minibatch(self,inputs,targets,batch_size):
+        """
+        A generator for batches of the input data
+
+        Args:
+            inputs (array[float]): input data
+            targets (array[float]): targets
+
+        Returns:
+            inputs (array[float]): one batch of input data of length `batch_size`
+            targets (array[float]): one batch of targets of length `batch_size`
+        """
+        for start_idx in range(0, inputs.shape[0], batch_size):
+            idxs = slice(start_idx, start_idx + batch_size)
+            yield inputs[idxs], targets[idxs]
+
+    
+    def fit(self,x_train,y_train,x_test = None,y_test = None, batch_size: int = 32, epochs: int = 30, validation_split = None, loss_weights = None, metric_weights = None):
+
+        if loss_weights is None:
+            loss_weights = np.array([i for i in range(self.n_inputs+1)],requires_grad = False)
+
+        if metric_weights is None:
+            metric_weights = np.array([i for i in range(self.n_inputs+1)],requires_grad = False)
 
         
+        # Save training data 
+        loss_train_history = np.zeros(epochs+1,requires_grad = False)
+        metric_train_history = np.zeros(epochs+1,requires_grad = False)
+        loss_test_history = np.zeros(epochs+1,requires_grad = False)
+        metric_test_history = np.zeros(epochs+1,requires_grad = False)
+
+        # Define optimizer
+        learning_rate = 0.01
+        
+        # Train
+        y_train_pred  = self.predict(x_train) 
+        l2_train = np.sum(self.L2(y_train,y_train_pred),axis = 0)
+        
+        loss_train = np.dot(loss_weights,l2_train)
+        metric_train = np.dot(metric_weights,l2_train)
+        
+        loss_train_history[0] = loss_train
+        metric_train_history[0] = metric_train
+        
+        # Test
+        y_test_pred = self.predict(x_test)
+        l2_test = np.sum(self.L2(y_test,y_test_pred),axis = 0)
+            
+        loss_test = np.dot(loss_weights,l2_test) 
+        metric_test = np.dot(metric_weights,l2_test) 
+            
+        loss_test_history[0] = loss_test
+        metric_test_history[0] = metric_test
+            
+
+        print(f"Initial | L2 train: {l2_train} | Loss train: {loss_train} | Metric train: {metric_train} | L2 test: {l2_test} | Loss test: {loss_test} | Metric test: {metric_test}")
+
+        for it in range(epochs):
+            for x_batch, y_batch in self.minibatch(x_train, y_train, batch_size=batch_size):
+                #grad = self.optimizer.compute_grad(self.cost,(self.weights,x_batch, y_batch, loss_weights),{})
+                self.weights, _, _, _ = self.optimizer.step(self.cost,self.weights,x_batch, y_batch, loss_weights)
+            
+            # Train
+            y_train_pred  = self.predict(x_train) 
+            l2_train = np.sum(self.L2(y_train,y_train_pred),axis = 0)
+        
+            loss_train = np.dot(loss_weights,l2_train)
+            metric_train = np.dot(metric_weights,l2_train)
+            
+            loss_train_history[it+1] = loss_train
+            metric_train_history[it+1] = metric_train
+            
+            # Test
+            y_test_pred = self.predict(x_test)
+            l2_test = np.sum(self.L2(y_test,y_test_pred),axis = 0)
+            
+            loss_test = np.dot(loss_weights,l2_test) 
+            metric_test = np.dot(metric_weights,l2_test) 
+            
+            loss_test_history[it+1] = loss_test
+            metric_test_history[it+1] = metric_test
+        
+            print(f"Epoch: {it} | L2 train: {l2_train} | Loss train: {loss_train} | Metric train: {metric_train} | L2 test: {l2_test} | Loss test: {loss_test} | Metric test: {metric_test}")
+
+        return loss_train_history, loss_test_history, metric_train_history, metric_test_history
+
+
+    def plot(self):
+        print(qml.draw(self.circuit)(self.weights,[i for i in range(self.n_inputs+self.n_outputs)]))
+        return None
+
+def mesh(foo, test_size = 0.0):
+    # Define shuffled mesh
+    coordinates = np.array(np.meshgrid(*foo)).T.reshape(-1, len(foo))
+    np.random.shuffle(coordinates)
+    # Divide in validation and training
+    train_index = int(len(coordinates)*(1.-test_size))
+    input_train = coordinates[:train_index]
+    input_test = coordinates[train_index:]
+    return input_train, input_test
--- tests/test8.py
+++ tests/test8.py
@@ -11,52 +11,54 @@ import matplotlib.pyplot as plt
 # Data
 size = 10
 x = np.linspace(-1,1,size,requires_grad = False)
-input_train, input_test = mesh((x,), test_size = 0.2)
+y = np.linspace(-1,1,size,requires_grad = False)
+input_train, input_test = mesh((x,y), test_size = 0.2)
 
 # Define outputs
-output_train = np.sin(input_train[:,0])
-output_test = np.sin(input_test[:,0])
+output_train = np.sin(input_train[:,0])+np.sin(input_train[:,1]) 
+output_test = np.sin(input_test[:,0])+np.sin(input_test[:,1]) 
 output_train = output_train[:,None]
 output_test = output_test[:,None]
 
 output_train_grad0 = np.cos(input_train[:,0])[:,None]
+output_train_grad1 = np.cos(input_train[:,1])[:,None]
 
 output_test_grad0 = np.cos(input_test[:,0])[:,None]
+output_test_grad1 = np.cos(input_test[:,1])[:,None]
 
-output_train = np.hstack((output_train,output_train_grad0))
-output_test = np.hstack((output_test,output_test_grad0))
+output_train = np.hstack((output_train,output_train_grad0, output_train_grad1))
+output_test = np.hstack((output_test,output_test_grad0,output_test_grad1))
 
 # Test 1
-n_inputs = 1
+n_inputs = 2
 re_uploading_pqc = PQC(n_inputs,n_layers = 1)
 re_uploading_pqc.plot()
 re_uploading_pqc.compile()
 
-loss_weights = [0.5, 0.5]
-metric_weights = [1.0, 0.0]
+loss_weights = [0.33, 0.33, 0.33]
+metric_weights = [1.0, 0.0, 0.0]
 loss_train_history1, loss_test_history1, metric_train_history1, metric_test_history1 = re_uploading_pqc.fit(input_train,output_train,
                                                                                                             input_test,output_test,
-                                                                                                            epochs = 50,
+                                                                                                            epochs = 10,
                                                                                                             loss_weights = loss_weights, 
                                                                                                             metric_weights = metric_weights)
 
-print("\n\n#######################################################################################################\n\n")
 # Test 2
-n_inputs = 1
+n_inputs = 2
 re_uploading_pqc = PQC(n_inputs, n_layers = 1)
 re_uploading_pqc.compile()
 
-loss_weights = [1.0,0.0]
-metric_weights = [1.0,0.0]
+loss_weights = [1.0,0.0,0.0]
+metric_weights = [1.0,0.0,0.0]
 loss_train_history2, loss_test_history2, metric_train_history2, metric_test_history2 = re_uploading_pqc.fit(input_train,output_train,
                                                                                                             input_test,output_test,
-                                                                                                            epochs = 50,
+                                                                                                            epochs = 10,
                                                                                                             loss_weights = loss_weights, 
                                                                                                             metric_weights = metric_weights)
 
 # Plot
-plt.plot(metric_test_history1, label = "Gradients")
-plt.plot(metric_test_history2, label = "No gradients")
+plt.plot(metric_test_history1, label = "Gradient")
+plt.plot(metric_test_history2, label = "No gradient")
 plt.legend()
 plt.grid()
 plt.savefig('images/2d.png')
